4d713c3f39d10cdda1db38c872b30eff8d5966aa
Simrankarnwal <simrankarnwal111@gmail.com>
2023-02-21 12:44:41 +0530

    Styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from "./components/Navbar.js"
 import { BrowserRouter as Router } from "react-router-dom"; 
+import Slider from "./components/Slider.js"
+import data from "./data/data.json"
+import Offers from "./components/Offers.js"
 function App() {
   return (
    <Router>
     <PreNavbar />
     <Navbar />
+    <Slider start={data.banner.start}/>
+    <Offers Offer={data.offer} />
     </Router>
   );
 }
